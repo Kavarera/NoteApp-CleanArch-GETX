fc54bbdf1cfd3bbee34c7e6f171f27e2c61b5270
Kavarera <rafli.030715@gmail.com>
2025-01-27 07:40:42 +0700

    nothing to update

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:noteapp/core/configurations/custom_theme.dart';
-import 'package:noteapp/features/note_app/presentation/home/pages/home_page.dart';
 import 'package:noteapp/features/note_app/presentation/initial_binding.dart';
 import 'package:noteapp/features/note_app/presentation/splashscreen/pages/splashscreen_pages.dart';
 
