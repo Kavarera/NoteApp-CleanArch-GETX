13efaef5e8490be24a067f2ff47435e4b4c0c185
Kavarera <rafli.030715@gmail.com>
2025-01-28 08:06:21 +0700

    update init binding for category clean arch

--- lib/features/note_app/presentation/initial_binding.dart
+++ lib/features/note_app/presentation/initial_binding.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
 import 'package:noteapp/features/note_app/data/datasource/note_local_data_source.dart';
+import 'package:noteapp/features/note_app/data/repositories/category_repository_impl.dart';
 import 'package:noteapp/features/note_app/data/repositories/note_repository_impl.dart';
+import 'package:noteapp/features/note_app/domain/repositories/category_repository.dart';
 import 'package:noteapp/features/note_app/domain/repositories/note_repository.dart';
+import 'package:noteapp/features/note_app/domain/usecases/get_all_category_usecase.dart';
 import 'package:noteapp/features/note_app/domain/usecases/get_all_notes.dart';
 import 'package:noteapp/features/note_app/presentation/home/controllers/home_controller.dart';
 import 'package:noteapp/features/note_app/presentation/splashscreen/controllers/splashscreen_controller.dart';
@@ -16,10 +19,14 @@ class InitialBinding implements Bindings {
     // Repository
     Get.lazyPut<NoteRepository>(
         () => NoteRepositoryImpl(localDataSource: Get.find()));
+    Get.lazyPut<CategoryRepository>(
+        () => CategoryRepositoryImpl(localDataSource: Get.find()));
 
     //Use Cases
     Get.lazyPut<GetAllNotesUseCase>(
         () => GetAllNotesUseCase(noteRepository: Get.find()));
+    Get.lazyPut<GetAllCategoriesUseCase>(
+        () => GetAllCategoriesUseCase(categoryRepository: Get.find()));
 
     //Controllers
     Get.lazyPut<HomeController>(
